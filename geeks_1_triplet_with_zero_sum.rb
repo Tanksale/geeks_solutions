82e1e1b3f96ea0735b43b300ed0fe6fcbe463477
Amulya Tanksale <amulya@sedintechnologies.com>
2019-12-13 16:40:39 +0530

    added another way for finding triplet with zero sum

--- geeks_1_triplet_with_zero_sum.rb
+++ geeks_1_triplet_with_zero_sum.rb
@@ -11,10 +11,35 @@
 
 elements = eval(ARGV[0])
 
-elements.combination(3).to_a.each do |triplet|
-	sum = triplet[0] + triplet[1] + triplet[2]
-	puts triplet.inspect if sum.eql?(0)
+class TripletWithZeroSum
+  def self.ruby_way_1(elements)
+	  elements.combination(3).to_a.each do |triplet|
+		  sum = triplet[0] + triplet[1] + triplet[2]
+			puts triplet.inspect if sum.eql?(0)
+		end
+	end
+
+
+
+	# Another solution for the above problem
+
+	def self.way_2(elements)
+		element_size = elements.length
+		(0).upto(element_size-3) do |i|
+		  (i+1).upto(element_size-2) do |j|
+		    (j+1).upto(element_size-1) do |k|
+		      x = elements[i] + elements[j] + elements[k]
+			    if x == 0
+			      p [elements[i],elements[j],elements[k]]
+			      found = true
+			    end
+		    end
+		  end
+		end
+	end
 end
 
 # Commands to run this file
 # ruby geeks_1_triplet_with_zero_sum.rb "[0, -1, 2, -3, 1]"
+
+TripletWithZeroSum.way_2(elements)
