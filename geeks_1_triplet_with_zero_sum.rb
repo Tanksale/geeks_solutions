8214845bbc39ffd659a6f1586891cab9922923dd
Tanksale <amulya.tanksale@gmail.com>
2019-12-13 16:49:05 +0530

    Update geeks_1_triplet_with_zero_sum.rb

--- geeks_1_triplet_with_zero_sum.rb
+++ geeks_1_triplet_with_zero_sum.rb
@@ -13,30 +13,28 @@ elements = eval(ARGV[0])
 
 class TripletWithZeroSum
   def self.ruby_way_1(elements)
-	  elements.combination(3).to_a.each do |triplet|
-		  sum = triplet[0] + triplet[1] + triplet[2]
-			puts triplet.inspect if sum.eql?(0)
-		end
+    elements.combination(3).to_a.each do |triplet|
+      sum = triplet[0] + triplet[1] + triplet[2]
+      puts triplet.inspect if sum.eql?(0)
+    end
+  end
+
+  # Another solution for the above problem
+
+  def self.way_2(elements)
+    element_size = elements.length
+    (0).upto(element_size-3) do |i|
+      (i+1).upto(element_size-2) do |j|
+        (j+1).upto(element_size-1) do |k|
+	  x = elements[i] + elements[j] + elements[k]
+	  if x == 0
+	    p [elements[i],elements[j],elements[k]]
+	    found = true
+	  end
 	end
-
-
-
-	# Another solution for the above problem
-
-	def self.way_2(elements)
-		element_size = elements.length
-		(0).upto(element_size-3) do |i|
-		  (i+1).upto(element_size-2) do |j|
-		    (j+1).upto(element_size-1) do |k|
-		      x = elements[i] + elements[j] + elements[k]
-			    if x == 0
-			      p [elements[i],elements[j],elements[k]]
-			      found = true
-			    end
-		    end
-		  end
-		end
-	end
+      end
+    end
+  end
 end
 
 # Commands to run this file
